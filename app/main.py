3be02ba87d702996c5729133f5f708ebb84fd24b
yhgaoFAGBAT <yg6952@utexas.edu>
2022-12-28 14:31:48 -0500

    pass 2nd test

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 import socket
 
 
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -9,7 +10,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    # server_socket.accept() # wait for client
+    client_socket, client_address = server_socket.accept()
+    data = client_socket.recv(1024)
+    msg = b"+PONG\r\n"
+    client_socket.send(msg)
+    
 
 
 if __name__ == "__main__":
